b5114c82e478b8587fbaab2168339b3d73a2e1db
sanuss <sanussuser@gmail.com>
2020-12-02 13:27:09 +0100

    modifié

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
-class Voitures{
+class Voiture{
     constructor(immatriculation, couleur, poid, puissance, capaciteReservoir, niveauDessance, nombreDePlace, assure, msgTableaudeBord){
-        this.immatriculation = immatriculation,
-        this.couleur = couleur,
-        this.poid = poid,
-        this.puissance = puissance,
-        this.capaciteReservoir = capaciteReservoir,
-        this.niveauDessance = niveauDessance,
-        this.nombreDePlace = nombreDePlace,
-        this.assure = assure,
+        this.immatriculation = immatriculation
+        this.couleur = couleur
+        this.poid = poid
+        this.puissance = puissance
+        this.capaciteReservoir = capaciteReservoir
+        this.niveauDessance = niveauDessance
+        this.nombreDePlace = nombreDePlace
+        this.assure = assure
         this.msgTableaudeBord = msgTableaudeBord
     }
-        
-        repeindre(NouvelleCouleur) {
-           
-           if (this.couleur === NouvelleCouleur){
-               console.log("Merci pour ce rafrichissement!!");
-           }else{
-               this.couleur = NouvelleCouleur;
-               console.log("Merci pour cette nouvelle couleur!")
+    repeindre(NouvelleCouleur) {   
+        if (this.couleur === NouvelleCouleur){
+            console.log("Merci pour ce rafrichissement!!");
+        }else{
+            this.couleur = NouvelleCouleur;
+            console.log("Merci pour cette nouvelle couleur!");
            }
        }
     }
-
-
-let Mavoiture = new Voitures("12 slk 56", "Rouge", "1500 KG", 150 , "60 litres" , " 5 places")
-
+let Mavoiture = new Voiture("12 slk 56", "Rouge", 1500, 150, 60, 5)
 console.log(Mavoiture);
 Mavoiture.repeindre("grey");
 
